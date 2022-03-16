dc69903118c0cd2fe72ec2d25f77199ffb22002e
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2022-03-16 18:37:38 +0100

    handle game create request

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import websockets from './websockets'
+import bodyParser from 'body-parser'
 
 const app = express()
-const port = 3000
+app.use(bodyParser.json())
 
+const port = 3000
 const server = app.listen(port, () => {
   console.log("c'est parti")
   if (process.send) {
@@ -15,4 +17,10 @@ websockets(server)
 
 process.on('message', (message) => {
   console.log(message)
+})
+
+app.post('/create-game', (req, res) => {
+  const createGameRequest: { 'game-creator-name': string } = req.body
+  console.log({ 'game-creator-name': createGameRequest['game-creator-name'] })
+  res.send('coucou')
 })
